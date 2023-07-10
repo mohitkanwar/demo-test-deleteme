abb46cc25c5150d0b3842d98899be5dcda0e4a0b
Mohit Kanwar <Mohit.Kanwar@Xebia.com>
2023-07-10 12:06:24 +0530

    asd

---
+++ src/test/java/com/xebia/csek/demo/test/DemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xebia.csek.demo.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
