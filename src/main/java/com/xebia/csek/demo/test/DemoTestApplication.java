5808caae787e41fb6502e635530746954ce63b68
Mohit Kanwar <Mohit.Kanwar@Xebia.com>
2023-07-10 12:08:03 +0530

    adding xebia

--- src/main/java/com/xebia/csek/demo/test/DemoTestApplication.java
+++ src/main/java/com/xebia/csek/demo/test/DemoTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoTestApplication {
-
+	// invoke xebia test
 	public static void main(String[] args) {
 		SpringApplication.run(DemoTestApplication.class, args);
 	}
